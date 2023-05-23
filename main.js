f483b94a78a59f28d787fd458301d0caa6ec39fe
inacioX15 <inaciopassos5@gmail.com>
2023-05-23 11:26:14 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+
+function ativarNome(){
+    let nome = 'tamires';
+    console.log(nome)
+}
+
+function somarNumero(){
+   let a = 5;
+
+   let b = 10;
+   
+   let c = a*b;
+
+   console.log(c)
+}
+
+
+
+function espelhar(){
+    let p = 'RECINIA QUE VOLTA A O NORMAL :D "ESQUECIR DE LINKAR" FALTA DE CORAGEM!';
+
+    document.write(p)
+    console.log(p)
+
+}
+
+var passaword = window.prompt('qual a senha?')
+
+window.confirm('Seja Bem Vindo')
+
+document.write(`<h1> Seja Bem Vindo, ${nome}</h1>`)
+
+
